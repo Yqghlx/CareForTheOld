8e6fd037d3d3623a650308634186f7c5ff204058
yqgMacos <1272387446@qq.com>
2026-04-11 23:05:09 +0800

    feat: 定义业务枚举类型（UserRole/HealthType/MedicationStatus/Frequency）

---
+++ CareForTheOld/Models/Enums/HealthType.cs
@@ -0,0 +1,19 @@
+namespace CareForTheOld.Models.Enums;
+
+/// <summary>
+/// 健康数据类型枚举
+/// </summary>
+public enum HealthType
+{
+    /// <summary>血压</summary>
+    BloodPressure = 0,
+
+    /// <summary>血糖</summary>
+    BloodSugar = 1,
+
+    /// <summary>心率</summary>
+    HeartRate = 2,
+
+    /// <summary>体温</summary>
+    Temperature = 3
+}
